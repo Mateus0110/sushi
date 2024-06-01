03cfe86ca71cfd3787e40459e12b481aefa740af
Enzo Evangelista <enzoteu55@gmail.com>
2024-06-01 17:20:08 -0300

    ISS6 - Bug fixes
    Color correction, scrollbar correction after mobile menu expansion, menu position correction.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 const hamburguer = document.querySelector("header .hamburguer");
+const body = document.querySelector("body");
 const nav = document.querySelector("header nav");
 
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("show");
+  body.classList.toggle("noscroll");
 });
